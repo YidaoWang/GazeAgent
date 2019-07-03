077dcaed8f1b79fa23c6f719a9d0ae3bf7945eca
mryd <azelf.trickroom@gmail.com>
2019-07-03 10:16:08 +0900

    リファクタリング

--- Tobii Unity Project/Assets/TobiiPro/ScreenBased/Scripts/Player.cs
+++ Tobii Unity Project/Assets/TobiiPro/ScreenBased/Scripts/Player.cs
@@ -7,19 +7,19 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour
 {
-    private Agent agent;
+    private CommunicationMedia media;
 
     public void UpdateAgent(Rect rect, List<Vector2> landmarkPoints, Vector2 gazePoint)
     {
-        CmdDisplayAgent(netId, rect, landmarkPoints, gazePoint);
+        //CmdDisplayAgent(netId, rect, landmarkPoints, gazePoint);
     }
 
     public override void OnStartLocalPlayer()
     {
         print("OnStartLocalPlayer");
         base.OnStartLocalPlayer();
-        agent = GameObject.Find("Agent").GetComponent<Agent>();
-        agent.OnUpdateAgent += UpdateAgent;
+        media = GameObject.Find("CommunicationMedia").GetComponent<CommunicationMedia>();
+        media.OnUpdateAgent += UpdateAgent;
     }
 
     // Start is called before the first frame update
@@ -35,25 +35,25 @@ public class Player : NetworkBehaviour
     }
 
 
-    [Command]
-    void CmdDisplayAgent(NetworkInstanceId id, Rect rect, List<Vector2> landmarkPoints, Vector2 gazePoint)
-    {
-        print("cmd" + id.Value);
-        RpcDisplayAgent(id, rect, landmarkPoints, gazePoint);
-    }
+    //[Command]
+    //void CmdDisplayAgent(NetworkInstanceId id, Rect rect, List<Vector2> landmarkPoints, Vector2 gazePoint)
+    //{
+    //    print("cmd" + id.Value);
+    //    RpcDisplayAgent(id, rect, landmarkPoints, gazePoint);
+    //}
 
-    [ClientRpc]
-    void RpcDisplayAgent(NetworkInstanceId id, Rect rect, List<Vector2> landmarkPoints, Vector2 gazePoint)
-    {
-        print("rpc");
-        print("netid" + netId.Value);
-        print("id" + id.Value);
-        if (id.Value == netId.Value)
-        {
-            if(agent != null)
-            {
-                agent.SetAgent(rect, landmarkPoints, gazePoint);
-            }
-        }
-    }
+    //[ClientRpc]
+    //void RpcDisplayAgent(NetworkInstanceId id, Rect rect, List<Vector2> landmarkPoints, Vector2 gazePoint)
+    //{
+    //    print("rpc");
+    //    print("netid" + netId.Value);
+    //    print("id" + id.Value);
+    //    if (id.Value == netId.Value)
+    //    {
+    //        if(media != null)
+    //        {
+    //            media.SetAgent(rect, landmarkPoints, gazePoint);
+    //        }
+    //    }
+    //}
 }
---
+++ Tobii Unity Project/Assets/UDP/AgentData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.UDP
+{
+    public class AgentData
+    {
+        public Vector2 GazePoint { get; set; }
+        public Vector2[] FaceLandmark { get; set; }
+
+        public AgentData(Vector2[] faceLandmark, Vector2 gazePoint)
+        {
+            FaceLandmark = faceLandmark;
+            gazePoint = GazePoint;
+        }
+
+        public AgentData(byte[] agentData)
+        {
+            var floatArray = new float[agentData.Length / 4];
+            Buffer.BlockCopy(agentData, 0, floatArray, 0, agentData.Length);
+
+            GazePoint = new Vector2(floatArray[0], floatArray[1]);
+            FaceLandmark = new Vector2[floatArray.Length / 2 - 1];
+            for(var i = 0; i < FaceLandmark.Length; i++)
+            {
+                FaceLandmark[i] = new Vector2(floatArray[i * 2 + 2], floatArray[i * 2 + 3]);
+            }
+        }
+
+        public byte[] ToBytes()
+        {
+            var floatArray = new float[(FaceLandmark.Length + 1) * 2];
+
+            floatArray[0] = GazePoint.x;
+            floatArray[1] = GazePoint.y;
+
+            for (var i = 0; i < FaceLandmark.Length; i++)
+            {
+                floatArray[i * 2 + 2] = FaceLandmark[i].x;
+                floatArray[i * 2 + 3] = FaceLandmark[i].y;
+            }
+
+            var byteArray = new byte[floatArray.Length * 4];
+            Buffer.BlockCopy(floatArray, 0, byteArray, 0, byteArray.Length);
+
+            return byteArray;
+        }
+    }
+}
